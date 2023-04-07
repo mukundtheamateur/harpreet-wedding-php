a529005ae36329e0b228ad19809cf76f78e1ea6a
mukundtheamateur <mukundshukla000@gmail.com>
2023-04-07 20:53:28 +0530

    new

---
+++ constants/config.php
@@ -0,0 +1,35 @@
+<?php
+//Website Settings
+$site_title = "Dream Wedding";
+$about_site = "Before two hearts unite in celebration, a lot goes into preparations. Before the love is declared in public, there are many defining moments of choosing and picking.";
+
+date_default_timezone_set('Asia/Colombo');
+
+$currency = "LKR";
+$installation_path = "localhost/dreamwedding";
+
+
+//Social Network Setings
+$facebook_link = "#";
+$twitter_link = "#";
+$instagram_link = "#";
+$googleplus_link = "#";
+
+//Database Settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wedding";
+
+//Contact Settings
+$site_email = "dreamwedd.lk@gmail.com";
+$site_phone = "+94 78 461 1933";
+$site_address = "Mihintale, Anuradhapura";
+
+//SMTP Settings
+$smtp_host = 'smtp.gmail.com';
+$smtp_user = 'dreamwedd.lk@gmail.com';
+$smtp_pass = 'dimuthu2017';
+
+
+?>
